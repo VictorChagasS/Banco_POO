69dc271b051c6c62e3e4078eea2824a163f4f704
VictorChagasS <victorsantoschagas@hotmail.com>
2022-11-13 14:05:00 -0300

    finalizado com static

--- src/logic/Main.java
+++ src/logic/Main.java
@@ -7,15 +7,15 @@ public class Main{
             
                 new Login().setVisible(true);
     
-        Banco sant = new Banco("Santander", "033", 3);
+        Banco banese = new Banco("Banese", "033", 3);
         Banco brad = new Banco("Bradesco", "237", 3);
         Banco itau = new Banco("Itaú", "0500", 3);
-        SistemaBancos.add(sant);
+        SistemaBancos.add(banese);
         SistemaBancos.add(brad);
         SistemaBancos.add(itau);
 
        
-        ContaPF victor = new ContaPF("Victor Chagas","01234567891","123","victor@santander.com","28/11/2002");
+        ContaPF victor = new ContaPF("Victor Chagas","01234567891","123","victor@banese.com","28/11/2002");
         ContaPF gui = new ContaPF("Guilherme Argolo","01234564391","0101","gui@itau.com","28/11/2002");
         ContaPJ comicBook = new ContaPJ("Comic Store Book", "03.778.130/0001-48", "gibi12", "comic@bradesco.com");
 
@@ -24,7 +24,7 @@ public class Main{
         System.out.println(gui.getNumeroConta());
         System.out.println(comicBook.getNumeroConta());
       
-        SistemaBancos.register(victor, sant);
+        SistemaBancos.register(victor, banese);
         SistemaBancos.register(gui, itau);
         SistemaBancos.register(comicBook, brad);
             
